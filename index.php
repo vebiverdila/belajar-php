c55443f3f007eb1cfd700a315da952ac2af2b3b6
Vebi Verdila <vebyverdila24@gmail.com>
2022-10-24 11:55:33 +0800

    initial comit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Vebi";
+    $umur = 20;
+
+
+    echo "Nama saya <strong> $nama</strong>, saya berusia $umur tahun<br>";
+
+    $namaAdik ="byby";
+    $umurAdik = 10;
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+    $selisihUmur = $umur - $umurAdik;
+    echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun";
+
+?>
