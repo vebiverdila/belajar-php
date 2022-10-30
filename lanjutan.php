59ca1d6ab6dacb72577c16684c0bea439cbed4ad
Vebi Verdila <vebyverdila24@gmail.com>
2022-10-31 03:31:11 +0800

    menambahkan data baru

--- lanjutan.php
+++ lanjutan.php
@@ -1,63 +1,128 @@
 <?php
 
-$nama ="Vebi";
+// $nama = "Michael Klemens";
 
-// PERULANGAN
+/*
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+*/
+
+// Perulangan
+/*
+$no = 100;
+for ($i=0; $i<$no; $i++) {
+    $n = $i + 1;
+    echo $n." ".$nama."<br/>";
+}
+*/
+
+/*
 $no = 10;
-$i= 0; 
-// for ($i=0;$i<5; $i++) {
-//     $n = $i + 1;
-//     echo $n." "$nama<br/>";
-// }
+$i = 0;
+while ($i < $no) {
+     $n = $i + 1;
+    echo $n." ".$nama."<br/>";
+    $i++;
+}
+*/
 
+/*
+$no = 10;
+$i = 0;
+do {
+    $n = $i + 1;
+    echo $n." ".$nama."<br/>";
+    $i++;
+} while ($i < $no)
+*/
+ /*
+$data = array('Avanza', 'Lamborghini', 'Tesla', 'Xenia', 'XPander', 'Rubicon');
+$i = 0;
+while ($i < count($data)) {
+    echo $data[$i]."<br>";
+    $i++;
+}
+*/
+// echo $data[5];
 
-// while ($i < $no) {
-//     $n = $i + 1;
-//     echo $n." ".$nama."<br/>";
-//     $i++;
-// }
-
-// do {
-//     $n = $i + 1;
-//     echo $n." ".$nama."<br/>";
-//     $i++; 
-// } while ($i < $no)
-
-// $data= array('Avanza','Lamborghini', 'Tesla', 'Xenia', 'XPander', 'Rubicon');
-// foreach($data as $value) {
-//     echo $value."<br>";
-// }
-
-// $data= array('Avanza','Lamborghini', 'Tesla', 'Xenia', 'XPander', 'Rubicon');
-
-// $i= 0;
-// while ($i < count($data)) {
-//     echo $data[$i]."<br>";
-//     $i++;
-// }
-// for ($i=0; $i<count($data); $i++) {
-//     echo $data[$i]."<br>";
-// }
+/*
+foreach($data as $value) {
+    echo $value."<br>";
+}
+*/
 
 // PERCABANGAN
-// if ($nama == "Putu") {
-//     echo $nama." adalah orang Bali";
-// } else if($nama== "Budi") {
-//     echo $nama." berasal dari pulau Jawa";
-// } else {
-//     echo $nama. "dari mana ya?";
-// }
-
+/*
+if ($nama == "Michael Klemens") {
+    echo $nama." adalah orang Samarinda";
+} else if($nama == "Vebi") {
+    echo $nama." berasal dari Tana Tidung";
+} else {
+    echo $nama." darimana ya?";
+}
+*/
+/*
 switch($nama) {
-    case "Putu":
-        $pesan = $nama. " adalah orang Bali";
+    case "Michael Klemens":
+        $pesan = $nama." adalah orang Samarinda";
     break;
-    case "Budi":
-        $pesan = $nama. " adalah orang Jawa";
+    case "Vebi":
+        $pesan = $nama." berasal dari Tana Tidung";
     break;
     default:
-        $pesan =$nama." darimana ya?";
-        
+        $pesan = $nama." darimana ya?";
 }
 echo $pesan;
+*/
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Belajar</title>
+</head>
+<body>
+    <h1>Input Nama dan Diulang</h1>
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+        <label>Nama</label>
+        <input type="text" name="nama">
+        <label>Jumlah</label>
+        <input type="text" name="no">
+        <input type="submit" name="submit" value="Submit">
+    </form>
+    <?php
+        if(!empty($_POST['submit'])) {
+
+            switch($_POST['nama']) {
+                case "Michael Klemens":
+                    $pesan = $_POST['nama']." adalah orang Samarinda";
+                break;
+                case "Vebi":
+                    $pesan = $_POST['nama']." berasal dari Tana Tidung";
+                break;
+                default:
+                    $pesan = $_POST['nama']." darimana ya?";
+            
+            }
+
+            for ($i=0;$i<$_POST['no'];$i++) {
+                echo $pesan."<br>";
+            }
+
+        } else {
+            echo "Anda belum input nama dan jumlah.";
+        }
+    ?>
+</body>
+</html>
