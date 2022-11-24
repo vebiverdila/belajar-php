e3e8d68b09a9a1aac62263ffcbfb659ee00c6a93
Vebi Verdila <vebyverdila24@gmail.com>
2022-11-24 11:42:01 +0800

    menambahkan update dan hapus data

--- index.php
+++ index.php
@@ -1,37 +1,32 @@
 <?php
 
+// 1. Buat koneksi dengan MySQL
 $con = mysqli_connect("localhost","root","","fakultas");
 
-
+// 2. Check connection
 if (mysqli_connect_errno()) {
-    echo "Koneksi gagal". mysqli_connect_errno();
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
 }else{
-    echo "Koneksi berhasil";
+    echo 'koneksi berhasil';
 }
 
-// 3. membaca data
-$query = "SELECT * FROM mahasiswa";
+// 3 buat query baca semua data dari table
+$sql = "SELECT * FROM mahasiswa";
 
-// 4.menampilkan data
-$result = mysqli_query($con,$query);
-$mahasiswa= [] ;
-if ($result){
-    // tampilkan data satu persatu
-    while($row = mysqli_fetch_assoc($result)) {
-        $mahasiswa[]= $row;
+// 4. tampilkan data, cek apakah query bisa dijalankan
+$mahasiswa = [];
+if ($result = mysqli_query($con, $sql)) {
+    // tampilkan satu per satu
+    while ($row = mysqli_fetch_assoc($result)) {
+        $mahasiswa[] = $row;
     }
     mysqli_free_result($result);
-}
+  }
 
-// 5. Tutup mysql
+// 5. tutup koneksi
 mysqli_close($con);
-
-foreach($mahasiswa as $values) {
-    echo $values ["nama"];
-
-}
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -43,21 +38,31 @@ foreach($mahasiswa as $values) {
 </head>
 <body>
     <h1>Data Mahasiswa</h1>
-    <table border ="1" style="width:100%;">
+    <a href="insert.php">Tambah Data</a>
+    <table border=1 style="width: 100%;">
         <tr>
-            <th>Nim</th>
+            <th>NIM</th>
             <th>Nama</th>
+            <th>Jenis Kelamin</th>
+            <th>Tempat Lahir</th>
+            <th>Tanggal Lahir</th>
+            <th>Alamat</th>
+            <th>Action</th>
         </tr>
-        <?php foreach($mahasiswa as $values): ?>
-        <tr>
-            <td><?php echo $values["nim"] ?></td>
-            <td><?php echo $values["nama"] ?></td>
-        </tr>
+        <?php foreach ($mahasiswa as $row): ?>
+            <tr>
+                <td><?= $row['nim'] ?></td>
+                <td><?= $row['nama'] ?></td>
+                <td><?= $row['jenis_kelamin'] ?></td>
+                <td><?= $row['tempat_lahir'] ?></td>
+                <td><?= $row['tanggal_lahir'] ?></td>
+                <td><?= $row['alamat'] ?></td>
+                <td>
+                    <a href="update.php?id=<?= $row['id'] ?>" >Edit</a> | 
+                    <a href="delete.php?id=<?= $row['id'] ?>" >Delete</a>
+                </td>
+            </tr>
         <?php endforeach; ?>
     </table>
-
-
-
-    
 </body>
 </html>
--- insert.php
+++ insert.php
@@ -1,36 +1,36 @@
 <?php
+    if (isset($_POST['submit'])){
+        //var_dump($_POST);
+        $nim = $_POST['nim'];
+        $nama = $_POST['nama'];
+        $id_jurusan = $_POST['id_jurusan'];
+        $tpt_lahir = $_POST['tpt_lahir'];
+        $tgl_lahir = $_POST['tgl_lahir'];
+        $gender = $_POST['gender'];
+        $alamat = $_POST['alamat'];
 
-if (isset($_POST["submit"])){
-    $nim = $_POST['nim'];
-    $nama = $_POST['nama'];
-    $id_jurusan = $_POST['id_jurusan'];
-    $tpt_lahir = $_POST['tpt_lahir'];
-    $tgl_lahir = $_POST['tgl_lahir'];
-    $gender = $_POST['gender'];
-    $alamat = $_POST['alamat'];
+        // Buat koneksi dengan MySQL
+        $con = mysqli_connect("localhost","root","","fakultas");
 
-    $con = mysqli_connect("localhost","root","","fakultas");
+        // Check connection
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            exit();
+        }else{
+            echo 'koneksi berhasil';
+        }
 
+        $sql = "insert into mahasiswa (id_jurusan, nim, nama, jenis_kelamin, tempat_lahir, tanggal_lahir, alamat)
+        values ($id_jurusan,'$nim', '$nama', '$gender', '$tpt_lahir', '$tgl_lahir', '$alamat')";
 
-    if (mysqli_connect_errno()) {
-        echo "Koneksi gagal". mysqli_connect_errno();
-    }else{
-        echo "Koneksi berhasil";
+        if (mysqli_query($con, $sql)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($con);
+        }
+          
+        mysqli_close($con);
     }
-
-    // buat sql query untuk insert ke database
-    $sql = "insert into mahasiswa (id_jurusan, nim, nama, jenis_kelamin, tempat_lahir, tanggal_lahir, alamat)
-            values ($id_jurusan, '$nim', '$nama', '$gender', '$tpt_lahir', '$tgl_lahir', '$alamat')";
-    
-    if (mysqli_query($con, $sql)){
-        echo "Data baru berhasil ditambahkan";
-    }else{
-        echo "Ada Error ". mysqli_error();
-    }
-
-    mysqli_close($con);
-}
-    
 ?>
 
 <!DOCTYPE html>
@@ -39,18 +39,20 @@ if (isset($_POST["submit"])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Data Mahasiswa</title>
+    <title>Insert</title>
 </head>
 <body>
-    <form action="insert.php" method="post">
-        Nim: <input type="text" name="nim"><br>
+    <h1>Tambah Data</h1>
+
+    <form action="" method="post">
+        NIM: <input type="text" name="nim"><br>
         Nama: <input type="text" name="nama"><br>
         ID Jurusan: <input type="number" name="id_jurusan"><br>
         Jenis Kelamin: <input type="text" name="gender"><br>
         Tempat Lahir: <input type="text" name="tpt_lahir"><br>
         Tanggal Lahir (yyyy-mm-dd): <input type="text" name="tgl_lahir"><br>
         Alamat: <input type="text" name="alamat"><br>
-        <button type="submit" name="submit"> Tambah Data</button>
+        <button type="submit" name="submit">Tambah Data</button>
     </form>
 </body>
 </html>
